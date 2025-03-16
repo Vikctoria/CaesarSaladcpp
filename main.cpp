090d49faef378fd829c3c1ed0a38cc913b728f49
Vika <a9153153020@gmail.com>
2025-03-17 01:36:04 +0300

    Add Polybius square

--- main.cpp
+++ main.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <vector>
 
+using std::cin;
+using std::cout;
+using std::vector;
+
+vector <vector <char>> Polybius_square() {
+    /*
+    Функция, возвращающая квадрат Полибия:
+    A B C D E
+    F G H I K
+    L M N O P
+    Q R S T U
+    V W X Y Z
+    */
+    vector <vector <char>> square (5, vector <char> (5));
+    char letter;
+    for (int row = 0; row < 5; ++row) {
+        for (int column = 0; column < 5; ++column) {
+            letter = 'A' + row * 5 + column;
+            if (letter >= 'J') {
+                ++letter;
+            }
+            square[row][column] = letter;
+        }
+    }
+    return square;
+}
+
+int main() {
+    return 0;
+}
