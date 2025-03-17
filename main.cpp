b42c103629e1c2b4f243af98d2e3f31f6b943f20
Vika <a9153153020@gmail.com>
2025-03-17 19:45:56 +0300

    Add new encoder - Pol_square_method. Add comments

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 using std::cin;
 using std::cout;
@@ -16,25 +17,47 @@ vector <vector <char>> Polybius_square() {
     Q R S T U
     V W X Y Z
     */
-    vector <vector <char>> square (5, vector <char> (5));
+    vector <vector <char>> square(5, vector <char>(5));
     char letter;
     for (int row = 0; row < 5; ++row) {
         for (int column = 0; column < 5; ++column) {
             letter = 'A' + row * 5 + column;
-            if (letter >= 'J') {
-                ++letter;
-            }
-            square[row][column] = letter;
+            square[row][column] = letter + (letter >= 'J');
         }
     }
     return square;
 }
 
 string replace_j(string message) {
+    /*
+    Функция меняющая в сообщении все "J" на "I"
+    */
     while (message.find('J') != std::string::npos) {
         message.replace(message.find('J'), 1, 1, 'I');
     }
     return message;
+}
+
+string Pol_square_method_1(string message) {
+    /*
+    Функция шифрующая сообщение по одному из методов шифровки с помощью квадрата Полибия
+    */
+    std::transform(message.begin(), message.end(), message.begin(), toupper);
+    message = replace_j(message);
+    vector <vector <char>> polyb_square = Polybius_square();
+    string encrypt_message = "";
+    char new_let;
+    for (char let_to_replace : message) {
+        if (isalpha(let_to_replace)) {
+            int row = ((let_to_replace - 'A' - (let_to_replace >= 'J')) / 5 + 1) % 5;
+            int col = (let_to_replace - 'A' - (let_to_replace >= 'J')) % 5;
+            new_let = polyb_square[row][col];
+        } else {
+            new_let = let_to_replace;
+        }
+        encrypt_message.push_back(new_let);
+    }
+    return encrypt_message;
 }
 
 int main() {
